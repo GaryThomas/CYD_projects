27a9c1b4e9d1b8734a44a458c9346e7002c11b6b
Gary Thomas <gary@mlbassoc.com>
2026-04-14 16:52:19 -0600

    Found display settings that work for my boards

--- src/main.cpp
+++ src/main.cpp
@@ -404,7 +404,7 @@ void lv_create_main_gui(void) {
     lv_label_set_text(text_label_date, current_date.c_str());
     lv_obj_align(text_label_date, LV_ALIGN_CENTER, 70, -70);
     lv_obj_set_style_text_font((lv_obj_t *)text_label_date, &lv_font_montserrat_26, 0);
-    lv_obj_set_style_text_color((lv_obj_t *)text_label_date, lv_palette_main(LV_PALETTE_BROWN), 0);
+    lv_obj_set_style_text_color((lv_obj_t *)text_label_date, lv_palette_main(LV_PALETTE_TEAL), 0);
 
     lv_obj_t *weather_image_temperature = lv_image_create(lv_screen_active());
     lv_image_set_src(weather_image_temperature, &image_weather_temperature);
@@ -413,7 +413,6 @@ void lv_create_main_gui(void) {
     lv_label_set_text(text_label_temperature, String("      " + temperature + degree_symbol).c_str());
     lv_obj_align(text_label_temperature, LV_ALIGN_CENTER, 70, -25);
     lv_obj_set_style_text_font((lv_obj_t *)text_label_temperature, &lv_font_montserrat_24, 0);
-    lv_obj_set_style_text_color((lv_obj_t *)text_label_temperature, lv_color_white(), 0);
 
     lv_obj_t *weather_image_humidity = lv_image_create(lv_screen_active());
     lv_image_set_src(weather_image_humidity, &image_weather_humidity);
@@ -422,13 +421,11 @@ void lv_create_main_gui(void) {
     lv_label_set_text(text_label_humidity, String("   " + humidity + "%").c_str());
     lv_obj_align(text_label_humidity, LV_ALIGN_CENTER, 70, 20);
     lv_obj_set_style_text_font((lv_obj_t *)text_label_humidity, &lv_font_montserrat_24, 0);
-    lv_obj_set_style_text_color((lv_obj_t *)text_label_humidity, lv_color_white(), 0);
 
     text_label_weather_description = lv_label_create(lv_screen_active());
     lv_label_set_text(text_label_weather_description, weather_description.c_str());
     lv_obj_align(text_label_weather_description, LV_ALIGN_BOTTOM_MID, 0, -40);
     lv_obj_set_style_text_font((lv_obj_t *)text_label_weather_description, &lv_font_montserrat_18, 0);
-    lv_obj_set_style_text_color((lv_obj_t *)text_label_weather_description, lv_color_white(), 0);
 
     // Create a text label for the time and timezone aligned center in the bottom of the screen
     text_label_time_location = lv_label_create(lv_screen_active());
@@ -436,8 +433,7 @@ void lv_create_main_gui(void) {
                       String("Last Update: " + last_weather_update + "  |  " + location).c_str());
     lv_obj_align(text_label_time_location, LV_ALIGN_BOTTOM_MID, 0, -10);
     lv_obj_set_style_text_font((lv_obj_t *)text_label_time_location, &lv_font_montserrat_12, 0);
-    // lv_obj_set_style_text_color((lv_obj_t *)text_label_time_location, lv_palette_main(LV_PALETTE_GREY), 0);
-    lv_obj_set_style_text_color((lv_obj_t *)text_label_time_location, lv_color_white(), 0);
+    lv_obj_set_style_text_color((lv_obj_t *)text_label_time_location, lv_palette_main(LV_PALETTE_GREY), 0);
 
     lv_timer_t *timer = lv_timer_create(timer_cb, 60000, NULL);
     lv_timer_set_repeat_count(timer, -1);
