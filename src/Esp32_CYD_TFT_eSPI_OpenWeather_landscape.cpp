d7532356aaff36f5c18d9b2f30a661f4ee1dcec9
Gary Thomas <gary@mlbassoc.com>
2026-04-16 05:16:45 -0600

    Merge SD card support - not really used yet

--- src/Esp32_CYD_TFT_eSPI_OpenWeather_landscape.cpp
+++ src/Esp32_CYD_TFT_eSPI_OpenWeather_landscape.cpp
@@ -80,6 +80,8 @@ const char *PROGRAM_VERSION = "ESP32 CYD OpenWeatherMap LittleFS V02.1";
 
 #include <FS.h>
 #include <LittleFS.h>
+#include <SD.h>
+#include <SPI.h>
 
 #define AA_FONT_SMALL "fonts/NSBold15" // 15 point Noto sans serif bold
 #define AA_FONT_LARGE "fonts/NSBold36" // 36 point Noto sans serif bold
@@ -155,6 +157,172 @@ int rightOffset(String text, String sub);
 int splitIndex(String text);
 int getNextDayIndex(void);
 
+// ----------------------------
+// SD Reader pins (default VSPI pins)
+// ----------------------------
+// #define SD_SCK 18
+// #define SD_MISO 19
+// #define SD_MOSI 23
+// #define SD_CS 5
+
+// ----------------------------
+
+void listDir(fs::FS &fs, const char *dirname, uint8_t levels) {
+    Serial.printf("Listing directory: %s\n", dirname);
+
+    File root = fs.open(dirname);
+    if (!root) {
+        Serial.println("Failed to open directory");
+        return;
+    }
+    if (!root.isDirectory()) {
+        Serial.println("Not a directory");
+        return;
+    }
+
+    File file = root.openNextFile();
+    while (file) {
+        if (file.isDirectory()) {
+            Serial.print("  DIR : ");
+            Serial.println(file.name());
+            if (levels) {
+                listDir(fs, file.path(), levels - 1);
+            }
+        } else {
+            Serial.print("  FILE: ");
+            Serial.print(file.name());
+            Serial.print("  SIZE: ");
+            Serial.println(file.size());
+        }
+        file = root.openNextFile();
+    }
+}
+
+void createDir(fs::FS &fs, const char *path) {
+    Serial.printf("Creating Dir: %s\n", path);
+    if (fs.mkdir(path)) {
+        Serial.println("Dir created");
+    } else {
+        Serial.println("mkdir failed");
+    }
+}
+
+void removeDir(fs::FS &fs, const char *path) {
+    Serial.printf("Removing Dir: %s\n", path);
+    if (fs.rmdir(path)) {
+        Serial.println("Dir removed");
+    } else {
+        Serial.println("rmdir failed");
+    }
+}
+
+void readFile(fs::FS &fs, const char *path) {
+    Serial.printf("Reading file: %s\n", path);
+
+    File file = fs.open(path);
+    if (!file) {
+        Serial.println("Failed to open file for reading");
+        return;
+    }
+
+    Serial.print("Read from file: ");
+    while (file.available()) {
+        Serial.write(file.read());
+    }
+    file.close();
+}
+
+void writeFile(fs::FS &fs, const char *path, const char *message) {
+    Serial.printf("Writing file: %s\n", path);
+
+    File file = fs.open(path, FILE_WRITE);
+    if (!file) {
+        Serial.println("Failed to open file for writing");
+        return;
+    }
+    if (file.print(message)) {
+        Serial.println("File written");
+    } else {
+        Serial.println("Write failed");
+    }
+    file.close();
+}
+
+void appendFile(fs::FS &fs, const char *path, const char *message) {
+    Serial.printf("Appending to file: %s\n", path);
+
+    File file = fs.open(path, FILE_APPEND);
+    if (!file) {
+        Serial.println("Failed to open file for appending");
+        return;
+    }
+    if (file.print(message)) {
+        Serial.println("Message appended");
+    } else {
+        Serial.println("Append failed");
+    }
+    file.close();
+}
+
+void renameFile(fs::FS &fs, const char *path1, const char *path2) {
+    Serial.printf("Renaming file %s to %s\n", path1, path2);
+    if (fs.rename(path1, path2)) {
+        Serial.println("File renamed");
+    } else {
+        Serial.println("Rename failed");
+    }
+}
+
+void deleteFile(fs::FS &fs, const char *path) {
+    Serial.printf("Deleting file: %s\n", path);
+    if (fs.remove(path)) {
+        Serial.println("File deleted");
+    } else {
+        Serial.println("Delete failed");
+    }
+}
+
+void testFileIO(fs::FS &fs, const char *path) {
+    File file = fs.open(path);
+    static uint8_t buf[512];
+    size_t len = 0;
+    uint32_t start = millis();
+    uint32_t end = start;
+    if (file) {
+        len = file.size();
+        size_t flen = len;
+        start = millis();
+        while (len) {
+            size_t toRead = len;
+            if (toRead > 512) {
+                toRead = 512;
+            }
+            file.read(buf, toRead);
+            len -= toRead;
+        }
+        end = millis() - start;
+        Serial.printf("%u bytes read for %u ms\n", flen, end);
+        file.close();
+    } else {
+        Serial.println("Failed to open file for reading");
+    }
+
+    file = fs.open(path, FILE_WRITE);
+    if (!file) {
+        Serial.println("Failed to open file for writing");
+        return;
+    }
+
+    size_t i;
+    start = millis();
+    for (i = 0; i < 2048; i++) {
+        file.write(buf, 512);
+    }
+    end = millis() - start;
+    Serial.printf("%u bytes written for %u ms\n", 2048 * 512, end);
+    file.close();
+}
+
 bool tft_output(int16_t x, int16_t y, uint16_t w, uint16_t h, uint16_t *bitmap) {
     // Stop further decoding as image is running off bottom of screen
     if (y >= tft.height())
@@ -171,9 +339,39 @@ bool tft_output(int16_t x, int16_t y, uint16_t w, uint16_t h, uint16_t *bitmap) 
 **                          Setup
 ***************************************************************************************/
 void setup() {
+    bool SDCardPresent = false;
+    SPIClass spi = SPIClass(VSPI);
+
     Serial.begin(115200);
     delay(500);
     Serial.println(PROGRAM_VERSION);
+
+    if (SD.begin(SS, spi, 80000000)) {
+        uint8_t cardType = SD.cardType();
+
+        if (cardType == CARD_NONE) {
+            Serial.println("No SD card attached");
+        } else {
+            SDCardPresent = true;
+            Serial.print("SD Card Type: ");
+            if (cardType == CARD_MMC) {
+                Serial.println("MMC");
+            } else if (cardType == CARD_SD) {
+                Serial.println("SDSC");
+            } else if (cardType == CARD_SDHC) {
+                Serial.println("SDHC");
+            } else {
+                Serial.println("UNKNOWN");
+            }
+
+            uint64_t cardSize = SD.cardSize() / (1024 * 1024);
+            Serial.printf("SD Card Size: %lluMB\n", cardSize);
+
+            listDir(SD, "/", 2);
+        }
+    } else {
+        Serial.println("Card Mount Failed");
+    }
 
     // Set up PWM for the backlight
     // NEW Version for ESP32 Core 3.x
