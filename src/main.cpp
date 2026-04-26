8c0c3757c3114b9aa0822e16d8dd15a68c6b9cc5
Gary Thomas <gary@mlbassoc.com>
2026-04-25 18:05:07 -0600

    Add user prompt to erase NVS

--- src/main.cpp
+++ src/main.cpp
@@ -6,13 +6,28 @@ BoardCfg boardCfg;
 
 void setup() {
     Serial.begin(115200);
-    
+
+    sleep(5);
     Serial.println("\n\nThe time has come, the walrus said, to talk of many things...");
 
     boardCfg.begin(); // Generate new config if not found, or load existing config if valid
-    Serial.println("BoardCfg valid: " + String(boardCfg.valid));
-    Serial.println("BoardCfg GUID: " + String(boardCfg.guid));
-    boardCfg.dump();
+    boardCfg.dump("Base configuration:");
+    // Update the device name and save it to NVS if not set
+    boardCfg.end(); // Clean up resources
+    sleep(5);
+
+    Serial.print("Reset? (y/n): ");
+    while (!Serial.available()) {
+        delay(100);
+    }
+    char response = Serial.read();
+    if (response == 'y' || response == 'Y') {
+        boardCfg.begin(); // Start again to show reset functionality
+        boardCfg.reset(); // Reset config to defaults (generates new GUID)
+        boardCfg.dump("Reset configuration:");
+        boardCfg.end();
+    }
+    ESP.restart(); // Restart to show that reset config is retained across resets
 }
 
 void loop() {}
